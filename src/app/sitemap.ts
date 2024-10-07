4a0d942b1911f7b57d1efe0378f1c6bc043eaf2d
SlygoPika <hmorindg25@gmail.com>
2024-10-07 19:55:29 -0400

    test

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,6 @@ import { MetadataRoute } from "next";
 export default async function sitemap() : Promise<MetadataRoute.Sitemap>
 {
     const baseUrl = "https://www.montrealhcpp.ca";
-
     return [
         {
             url: `${baseUrl}/`,
